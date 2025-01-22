ed9a780240a32f5d9a2ea9c3c54d7cd2eb8bc5f6
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-01-22 14:40:38 +0530

    dummy data is being created for the time being.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 
-# Create your views here.
+# adding dummy data temproarily
+
+posts=[
+    {
+        'author': 'bro',
+        'title' : 'bullshit',
+        'content' : 'first shit content',
+        'date_posted' : 'February 10, 2025'
+    },
+    {
+        'author': 'sis',
+        'title' : 'cowshit',
+        'content' : 'second shit content',
+        'date_posted' : 'April 06, 2025'
+    }
+]
+
 def home(request):
     return render(request, 'blog/home.html')
 
