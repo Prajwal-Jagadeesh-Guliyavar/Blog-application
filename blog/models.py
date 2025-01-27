102496fc9aec002df8f091aef1e6ac49128b0bbc
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-01-27 20:55:12 +0530

    imported the User and added the Foreignkey to have the authorities over the post so that when deletedthe user doesn't get deleted but hte post instead

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,3 +10,4 @@ class Post(models.Model):
     #date_posted = models.DateTimeField(auto_now_add=True)
     #the above statement will let the post have the date and time when published but it cannot be modified after the creation of the future
     date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
