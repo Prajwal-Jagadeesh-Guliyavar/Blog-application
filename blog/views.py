c5dc0e3a78b434703ec2fcf5a70d69d6eb478ce2
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-01-27 23:03:58 +0530

    using the created data instead of the dummy dictionary data

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
 
 # adding dummy data temproarily
@@ -20,7 +21,7 @@ posts=[
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
