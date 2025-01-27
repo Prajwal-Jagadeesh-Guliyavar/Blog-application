96bc1341d848833bbcaa6fabb27ffebd0ac910a7
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-01-27 23:19:54 +0530

    just getting rid of the dummy data.

--- blog/views.py
+++ blog/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render
 from .models import Post
 #from django.http import HttpResponse
 
-# adding dummy data temproarily
-
-posts=[
-    {
-        'author': 'bro',
-        'title' : 'bullshit',
-        'content' : 'first shit content',
-        'date_posted' : 'February 10, 2025'
-    },
-    {
-        'author': 'sis',
-        'title' : 'cowshit',
-        'content' : 'second shit content',
-        'date_posted' : 'April 06, 2025'
-    }
-]
-
 def home(request):
     context={
         'posts':Post.objects.all()
