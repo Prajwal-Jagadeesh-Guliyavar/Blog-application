4e2978bd915781facdfa1d94e8dbfb7ef3b589c8
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-03-05 11:35:29 +0530

    to store the profile pics in the custom directory

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +14,6 @@ urlpatterns = [
     path('logout/',users_views.logout_view, name = 'logout'), 
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
