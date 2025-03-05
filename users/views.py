c0a5bc955979b1fd7e669784c3095ec3dce7d02d
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-03-05 16:10:55 +0530

    form validations for the user and profiles

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request, f'{username}, you got your account Nigga, now Login and enjoy...')
+            messages.success(request, f'{username}, you got your account Bruv, now Login and enjoy...')
             return redirect('login')
     else:
         form=UserRegisterForm()
@@ -23,12 +23,26 @@ def logout_view(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance = request.user)
+        p_form = ProfileUpdateForm(
+                                request.POST,
+                                request.FILES,
+                                instance = request.user.profile
+                            )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Updating the profile doesnt make you look any smarter man')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
 
     context = {
         'u_form' : u_form,
         'p_form' : p_form
     }
 
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', context)
