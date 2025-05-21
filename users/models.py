87d314971027b6c4a6aaaa87ff9eedd0c4e1034a
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-05-21 23:56:41 +0530

    added the text field and to try teh erroe logging in the end of the file

--- users/models.py
+++ users/models.py
@@ -5,16 +5,20 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio = models.TextField(blank= True)
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.user.username} Profile"
-    
-    def save(self):
-        super().save()
-        img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            try:
+                img = Image.open(self.image.path)
+                if img.height > 300 or img.width > 300:
+                    output_size = (300, 300)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
+            except Exception as e:
+                # Optionally log the error
+                pass
