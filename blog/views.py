2f27702acc7a0594c19f0ca6a1efb53b7e09a151
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-05-24 16:20:09 +0530

    added the class based view for the creation of the post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 #from django.http import HttpResponse
 
@@ -18,6 +24,9 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 def about(request):
     return render(request, 'blog/about.html',{'title':'About'})
